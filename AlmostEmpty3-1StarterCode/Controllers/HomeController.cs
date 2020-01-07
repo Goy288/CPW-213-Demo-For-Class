3348dca20c862e9b06d6bb8d74330c3c67f5070f
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-01-07 11:27:37 -0800

    haha funny 87 meme

---
+++ AlmostEmpty3-1StarterCode/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmpty3_1StarterCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Index";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+            return View();
+        }
+    }
+}
